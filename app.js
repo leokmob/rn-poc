09ec044d815c338541a58b4bdc88ec679ee59b4f
leokmob <lkudryavtsev@mobquityinc.com>
2012-01-02 13:07:08 -0500

    Change port

--- app.js
+++ app.js
@@ -109,5 +109,5 @@ app.get('/sendSMS', function(req, res){
 	});	
 });
 
-app.listen(4000);
+app.listen(3001);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);

82dca64527675f1599e4471cc81ecf846eed91e6
leokmob <lkudryavtsev@mobquityinc.com>
2012-01-19 22:51:06 -0500

    Added test database call rendered in web page

--- app.js
+++ app.js
@@ -117,6 +117,27 @@ app.get('/select', function(req, res){
 	});
 });
 
+app.get('/selectdb', function(req, res){
+	client.query(
+	  '	select rn.RedNoteID, mc.Name, mc.Lyrics, mc.Mood, mc.SubMood, mc.TimeMS, mc.FileURL, os.Genre from RedNote rn '+
+		    ' join MusicClip mc on rn.MusicClipID = mc.MusicClipID' +
+		    ' join OriginalSong os on mc.OriginalSongID = os.OriginalSongID;',
+	    function selectCb(err, results, fields) {
+		    if (err) {
+		      res.end("Error" + err);
+		    }
+			else {
+				res.render('selectpagedb',{
+					title: "Select Red Note",
+					redNotes: results
+				});
+			}
+		    client.end();
+	    }
+	);
+});
+
+
 app.get('/playPage/:text/:from', function(req, res){
 	console.log(req.params);
   	res.render('playpage', {
